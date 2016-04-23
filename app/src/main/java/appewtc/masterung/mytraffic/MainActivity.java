7eaa48f4b71fd0e822359766b72e946e66b8d80f
masterUNG <phrombutr@gmail.com>
2016-04-23 15:05:54 +0700

    Finish 23April16

--- app/src/main/java/appewtc/masterung/mytraffic/MainActivity.java
+++ app/src/main/java/appewtc/masterung/mytraffic/MainActivity.java
@@ -1,13 +1,48 @@
 package appewtc.masterung.mytraffic;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit ประกาศตัวแปร
+    private ListView trafficListView;
+    private Button aboutButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Bind Widget
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutButton = (Button) findViewById(R.id.button);
+
+        //Button Controller
+        buttonController();
+
+
+    }   // Main Method
+
+    private void buttonController() {
+
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Sound Effect
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.cow);
+                mediaPlayer.start();
+
+            }   // onClick
+        });
+
+    }   // buttonController Method
+
+
+}   // Main Class นี่คือคลาสหลัก
